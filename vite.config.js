ce768277a03c430ac54c1c8cb13e6eda6440919a
yogeshPMishra <mail.yogeshpmishra@gmail.com>
2023-04-13 10:05:21 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : 'https://yogeshpmishra.github.io/demo_react_webapp/',
+  base : '/demo_react_webapp',
   plugins: [react()],
 })
